89962c83249987587ec1be13f7865fca49f37956
jhanc03 <jhancock1303@gmail.com>
2024-04-30 15:49:38 +0100

    Installed some packages

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'; 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SocialScreen from './screens/SocialScreen';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 //https://docs.expo.dev/versions/latest/sdk/location/
 import * as Location from 'expo-location';
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Social" component={SocialScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    );
+  }
+
 
 const styles = StyleSheet.create({
   container: {
